ab0eb81cbc0b91019b3bef403aa706532c3b6876
OslothLAS <osilveoruiz@frba.utn.edu.ar>
2025-04-21 14:16:15 -0300

    Solicitud, justificar solicitud y error

---
+++ src/main/java/entities/eliminacion/Contribuyente.java
@@ -0,0 +1,7 @@
+package entities.eliminacion;
+
+public class Contribuyente {
+    private String nombre;
+    private String apellido;
+    private Integer edad;
+}
--- src/main/java/entities/eliminacion/SolicitudEliminacion.java
+++ src/main/java/entities/eliminacion/SolicitudEliminacion.java
@@ -3,15 +3,23 @@ package entities.eliminacion;
 import entities.hechos.Hecho;
 
 public class SolicitudEliminacion {
-    //private Contribuyente solicitante;
-    // Falta hacer todo respecto al contribuyente
+    private Contribuyente solicitante;
+
     private String justificacion;
     private EstadoSolicitudEliminacion estado;
     private Hecho hecho;
 
-    public SolicitudEliminacion(String justificacion, Hecho hecho) {
-        this.justificacion = justificacion;
-        this.estado = EstadoSolicitudEliminacion.PENDIENTE;
-        this.hecho = hecho;
+    public void cambiarEstadoHecho(){
+        this.estado = EstadoSolicitudEliminacion.ACEPTADA;
+    }
+
+    public void justificarSolicitud(String justificacionSolicitud) {
+        if (justificacionSolicitud == null || justificacionSolicitud.length() < 500) {
+            throw new IllegalArgumentException("La justificacion debe tener al menos 500 caracteres");
+        }
+        else{
+            this.justificacion = justificacionSolicitud;
+        }
+
     }
 }
