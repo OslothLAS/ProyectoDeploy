ae9c787b73572ddcdc2fcb2ad10f0ca682f1b5c9
OslothLAS <osilveoruiz@frba.utn.edu.ar>
2025-04-22 09:30:16 -0300

    Creo una nueva rama

--- src/main/java/entities/eliminacion/SolicitudEliminacion.java
+++ src/main/java/entities/eliminacion/SolicitudEliminacion.java
@@ -1,24 +1,42 @@
 package entities.eliminacion;
 
 import entities.hechos.Hecho;
+import lombok.Getter;
 
+import java.time.LocalDateTime;
+
+@Getter
 public class SolicitudEliminacion {
     private Contribuyente solicitante;
-
+    private LocalDateTime fechaDeCreacion;
     private String justificacion;
     private EstadoSolicitudEliminacion estado;
     private Hecho hecho;
 
-    public void cambiarEstadoHecho(){
-        this.estado = EstadoSolicitudEliminacion.ACEPTADA;
-    }
 
-    public void justificarSolicitud(String justificacionSolicitud) {
+
+    public String justificarSolicitud(String justificacionSolicitud) {
         if (justificacionSolicitud == null || justificacionSolicitud.length() < 500) {
             throw new IllegalArgumentException("La justificacion debe tener al menos 500 caracteres");
         }
         else{
             this.justificacion = justificacionSolicitud;
         }
+        return justificacionSolicitud;
     }
+
+    public SolicitudEliminacion(String justificacion, Hecho hecho, Contribuyente solicitante) {
+        this.justificacion = this.justificarSolicitud(justificacion);
+        this.estado = EstadoSolicitudEliminacion.PENDIENTE;
+        this.hecho = hecho;
+        this.solicitante = solicitante;
+        this.fechaDeCreacion = LocalDateTime.now();
+    }
+
+    public void estadoDeSolicitud(EstadoSolicitudEliminacion estado) {
+        this.estado = EstadoSolicitudEliminacion.valueOf(estado.name());
+    }
+
+
+
 }
--- src/test/java/entities/eliminacion/SolicitudEliminacionTest.java
+++ src/test/java/entities/eliminacion/SolicitudEliminacionTest.java
@@ -1,46 +1,49 @@
 package entities.eliminacion;
 
+import entities.hechos.DatosHechos;
+import entities.hechos.Hecho;
+import entities.hechos.Origen;
+import entities.hechos.Ubicacion;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
+import static entities.eliminacion.EstadoSolicitudEliminacion.ACEPTADA;
+import static entities.hechos.Origen.CARGA_MANUAL;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolicitudEliminacionTest {
 
     @Test
-    public void testJustificarSolicitud_Exitoso() {
+    public void testCrearSolicitudPendiente() {
+        Ubicacion ubicacion = new Ubicacion("-32.786098","-60.741543");
+        DatosHechos datos =  new DatosHechos(
+                "Brote de enfermedad contagiosa causa estragos en San Lorenzo, Santa Fe",
+                "Grave brote de enfermedad contagiosa ocurrió en las inmediaciones de San Lorenzo, Santa Fe. El incidente dejó varios heridos y daños materiales. Se ha declarado estado de emergencia en la región para facilitar la asistencia.",
+                "Evento sanitario",
+                          ubicacion,
+                LocalDate.of(2005, 7, 5),
+                LocalDate.now(),
+                CARGA_MANUAL
+        );
 
-        SolicitudEliminacion solicitud = new SolicitudEliminacion();
         String justificacion = "a".repeat(500);
 
-        assertDoesNotThrow(() -> solicitud.justificarSolicitud(justificacion));
+        Hecho hecho = new Hecho();
+        hecho.setAutor("Osmar");
+        hecho.setDatosHechos(datos);
+
+        Contribuyente contribuyente = new Contribuyente();
+
+        SolicitudEliminacion solicitud = new SolicitudEliminacion(
+                justificacion,
+                hecho,
+                contribuyente
+        );
+        assertEquals(solicitud.getEstado(), EstadoSolicitudEliminacion.PENDIENTE);
+
+        solicitud.estadoDeSolicitud(ACEPTADA);
+        assertEquals(solicitud.getEstado(), ACEPTADA);
     }
 
-    @Test
-    public void testJustificarSolicitud_DemasiadoCorto() {
-        SolicitudEliminacion solicitud = new SolicitudEliminacion();
-        String justificacionCorta = "a".repeat(499);
-
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-                () -> solicitud.justificarSolicitud(justificacionCorta));
-
-        assertEquals("La justificacion debe tener al menos 500 caracteres", exception.getMessage());
-    }
-
-    @Test
-    public void testJustificarSolicitud_Null() {
-        SolicitudEliminacion solicitud = new SolicitudEliminacion();
-
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-                () -> solicitud.justificarSolicitud(null));
-
-        assertEquals("La justificacion debe tener al menos 500 caracteres", exception.getMessage());
-    }
-
-    @Test
-    public void testJustificarSolicitud_MasDe500Caracteres() {
-        SolicitudEliminacion solicitud = new SolicitudEliminacion();
-        String justificacionLarga = "a".repeat(501);
-
-        assertDoesNotThrow(() -> solicitud.justificarSolicitud(justificacionLarga));
-    }
 }
