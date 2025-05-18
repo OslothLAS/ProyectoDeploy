98403763f36c80807bde0283a8a2b3916267351c
OslothLAS <osilveroruiz@frba.utn.ar>
2025-05-17 23:08:36 -0300

    Agrego solicitud para empezar a trabajar

---
+++ solicitudes/src/test/java/mi_no_grupo_23/Solicitudes/SolicitudesApplicationTests.java
@@ -0,0 +1,13 @@
+package mi_no_grupo_23.Solicitudes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolicitudesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
