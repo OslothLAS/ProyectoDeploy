1735a96c4d5cd06af4560f17426ad50c19114627
gadstamati <gadstam71@gmail.com>
2025-05-27 14:03:25 -0300

    metaMapa added

---
+++ fuenteMetaMapa/src/test/java/frba/utn/ar/metamapa/metaMapa/MetaMapaApplicationTests.java
@@ -0,0 +1,13 @@
+package frba.utn.ar.metamapa.metaMapa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaMapaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
