1f14cedf9ee0fc92a30f673ab6509836cd61aaff
OslothLAS <osilveroruiz@frba.utn.ar>
2025-09-09 02:04:34 -0300

    Terminado el normalizador de ubicacion y categoria. (me olvide del run de spring)

--- agregador/src/main/java/ar/utn/frba/ddsi/agregador/AgregadorApplication.java
+++ agregador/src/main/java/ar/utn/frba/ddsi/agregador/AgregadorApplication.java
@@ -1,6 +1,7 @@
 package ar.utn.frba.ddsi.agregador;
 
 import entities.normalizador.GeoRefService;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import entities.normalizador.OpenStreetMap;
@@ -11,15 +12,19 @@ import java.util.List;
 public class AgregadorApplication {
 
 	public static void main(String[] args) {
+		SpringApplication.run(AgregadorApplication.class, args);
+
+
 		OpenStreetMap osm = new OpenStreetMap();
 		GeoRefService geo = new GeoRefService();
 
 		// Ejemplo 1: Obelisco de Buenos Aires
-		double lat1 = -34.6037;
-		double lon1 = -58.3816;
+		double lat1 = -34.914536;
+		double lon1 = -60.035774;
 		List<String> resultado1 = osm.obtenerUbicacion(lat1, lon1);
-		System.out.println("Provincia -> " + resultado1.get(0));
-		System.out.println("Obelisco -> " + resultado1.get(1));
+		System.out.println("Chivilcoy en alerta por Emanación de gas tóxico");
+		System.out.println("Buenos Aires -> " + resultado1.get(0));
+		System.out.println("Chivilcoy -> " + resultado1.get(1));
 
 
 		List<String> resultado2 = geo.obtenerUbicacion(lat1, lon1);
