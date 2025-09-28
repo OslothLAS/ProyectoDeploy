7054d5238aa56fd7af686fa4581c3d9f10938d1f
FrancoYsrraelit <fysrraelit@frba.utn.edu.ar>
2025-09-28 19:35:34 -0300

    agregado front

---
+++ frontMetaMapa/src/main/java/com/frontMetaMapa/frontMetaMapa/FrontMetaMapaApplication.java
@@ -0,0 +1,13 @@
+package com.frontMetaMapa.frontMetaMapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontMetaMapaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontMetaMapaApplication.class, args);
+	}
+
+}
---
+++ frontMetaMapa/src/test/java/com/frontMetaMapa/frontMetaMapa/FrontMetaMapaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frontMetaMapa.frontMetaMapa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontMetaMapaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
