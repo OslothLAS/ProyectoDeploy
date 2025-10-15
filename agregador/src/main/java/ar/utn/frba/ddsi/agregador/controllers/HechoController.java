b6c594692d3d92c0df1783632b89f590b89adc72
OslothLAS <osilveoruiz@frba.utn.edu.ar>
2025-10-15 15:36:25 -0300

    Actualizacion, ya no se duplican las hecho_coleccion

--- agregador/src/main/java/ar/utn/frba/ddsi/agregador/controllers/HechoController.java
+++ agregador/src/main/java/ar/utn/frba/ddsi/agregador/controllers/HechoController.java
@@ -1,12 +1,17 @@
 package ar.utn.frba.ddsi.agregador.controllers;
 
 import ar.utn.frba.ddsi.agregador.dtos.output.HechoOutputDTO;
+import ar.utn.frba.ddsi.agregador.models.entities.hechos.Provincia;
+import ar.utn.frba.ddsi.agregador.models.repositories.IProvinciaRepository;
 import ar.utn.frba.ddsi.agregador.services.IColeccionService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/hechos")
@@ -21,4 +26,7 @@ public class HechoController {
     public List<HechoOutputDTO> obtenerTodosLosHechos(){
         return this.coleccionService.obtenerTodosLosHechos();
     }
+
+
+
 }
