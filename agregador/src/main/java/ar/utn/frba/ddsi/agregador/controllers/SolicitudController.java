8ff9c2024b3de63383e6ee690d5a513ff2df98dc
OslothLAS <osilveoruiz@frba.utn.edu.ar>
2025-10-15 21:03:41 -0300

    Probando esto

--- agregador/src/main/java/ar/utn/frba/ddsi/agregador/controllers/SolicitudController.java
+++ agregador/src/main/java/ar/utn/frba/ddsi/agregador/controllers/SolicitudController.java
@@ -4,8 +4,12 @@ package ar.utn.frba.ddsi.agregador.controllers;
 import ar.utn.frba.ddsi.agregador.dtos.input.SolicitudInputDTO;
 import ar.utn.frba.ddsi.agregador.dtos.output.SolicitudOutputDTO;
 import ar.utn.frba.ddsi.agregador.services.ISolicitudEliminacionService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 import static ar.utn.frba.ddsi.agregador.utils.SolicitudUtil.solicitudToDTO;
 
 @RestController
@@ -37,5 +41,11 @@ public class SolicitudController {
     @PutMapping("/{id_solicitud}/rechazar")
     @PreAuthorize("hasRole('ADMIN')")
     public void rechazarSolicitud(@PathVariable("id_solicitud") Long idSolicitud) {this.solicitudService.rechazarSolicitud(idSolicitud);}
+
+    @GetMapping("hasRole('ADMIN')")
+    public ResponseEntity<List<SolicitudOutputDTO>> getSolicitudes(){
+
+        return ResponseEntity.ok(this.solicitudService.getSolicitudes());
+    }
 
 }
