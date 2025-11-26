859456101b04f98938100e258bf54ac1ab7f7a4f
FrancoYsrraelit <fysrraelit@frba.utn.edu.ar>
2025-11-26 11:05:23 -0300

    sacando harcodeo

--- agregador/src/main/java/ar/utn/frba/ddsi/agregador/AgregadorApplication.java
+++ agregador/src/main/java/ar/utn/frba/ddsi/agregador/AgregadorApplication.java
@@ -1,5 +1,7 @@
 package ar.utn.frba.ddsi.agregador;
 
+import ar.utn.frba.ddsi.agregador.config.FuenteConfig;
+import ar.utn.frba.ddsi.agregador.config.FuentesProperties;
 import ar.utn.frba.ddsi.agregador.models.entities.colecciones.Fuente;
 import ar.utn.frba.ddsi.agregador.models.entities.criteriosDePertenencia.CriterioDePertenencia;
 import ar.utn.frba.ddsi.agregador.models.entities.hechos.Hecho;
@@ -9,15 +11,18 @@ import ar.utn.frba.ddsi.agregador.services.IColeccionService;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
+@EnableConfigurationProperties(FuentesProperties.class)
 public class AgregadorApplication {
 
     @PersistenceContext
@@ -57,22 +62,14 @@ public class AgregadorApplication {
 
 
     @Bean
-    CommandLineRunner importarHechosFuentes(IHechoRepository hechoRepository, IColeccionService coleccionService) {
+    CommandLineRunner importarHechosFuentes(IHechoRepository hechoRepository, IColeccionService coleccionService, @Qualifier("fuentesProperties") FuentesProperties fuentesProperties) {
         return args -> {
-            List<Fuente> fuentes = List.of(
-                    new Fuente("localhost", "8060", null)
-            );
-/*
-,
-                    new Fuente("localhost", "8070", null),
-                    new Fuente("localhost", "8090", null)
- */
             List<CriterioDePertenencia> criterios = new ArrayList<>();
 
-            for (Fuente fuente : fuentes) {
+            for (FuenteConfig fuenteConfig : fuentesProperties.getFuentes()) {
+                Fuente fuente = new Fuente(fuenteConfig.getIp(), fuenteConfig.getPuerto(), null);
                 try {
                     List<Hecho> hechos = fuente.obtenerHechos(criterios);
-
                     coleccionService.normalizarHechos(hechos);
 
                     if (hechos != null && !hechos.isEmpty()) {
